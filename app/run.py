c5c6d39ee1d675b9c81bf0fa936e63d65890b685
gvkazare <germank.mail@gmail.com>
2020-06-21 16:15:10 +0300

    .

---
+++ app/run.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return "Flask works!"
+
+
+@app.route('/client/info', methods=['GET'])
+def client_info():
+    return {
+        'content-type': 'application/json',
+        'user_agent': str(request.user_agent)
+    }
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000)
